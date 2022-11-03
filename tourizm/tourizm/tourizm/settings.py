8634d746ee3a4375b317e9f7e4bb34c7abfff382
MikhailAtamanyuk <atamanmisha250595@gmail.com>
2022-11-03 16:32:10 +0500

    diplom

--- tourizm/tourizm/tourizm/settings.py
+++ tourizm/tourizm/tourizm/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'pesonal_account.apps.PesonalAccountConfig',
+    'crm.apps.CrmConfig',
+    'bot.apps.Telebot1Config',
+    'weather.apps.WeatherConfig',
 ]
 
 MIDDLEWARE = [
